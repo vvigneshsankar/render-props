9dafe4e0a9d317bb9be4fb9002aea869fbf9092c
Vignesh <vvigneshsankar@gmail.com>
2019-04-01 15:28:03 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Toggle from './Toggle';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <Toggle>
+       {({on,toggle})=>(
+     <div>
+      {on && <h1>Show Me</h1>}
+      <button onClick={toggle}>Show/Hide</button>
+      </div>
+    )}      
+    </Toggle>
       </div>
     );
   }
---
+++ src/ToggleRenderProps.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+
+export default class ToggleRenderProps extends Component{
+  state={
+    on:false
+  }
+  toggle=()=>{
+    this.setState({
+      on:!this.state.on
+    });
+  };
+  render()
+  {
+    const {render}=this.props
+    return(
+      <div>
+       {
+         render({
+           on:this.state.on,
+           toggle:this.toggle
+         }) 
+       }
+      </div>
+    );
+  }
+
+}
